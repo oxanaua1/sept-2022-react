e80fff39b52911a815e3efeaa2bc0e692df32d3c
OksanaKotsur <oxanaua1@gmail.com>
2023-01-24 21:36:14 +0200

    lesson3_hw_Forms1

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -23,9 +23,20 @@ const CarForm = () => {
             {errors.brand && <span>{errors.brand.message}</span>}
 
             <input type="text" placeholder={'price'} {...register('price',
-                {valueAsNumber: true})}/>
+                {
+                    valueAsNumber: true,
+                    min: {value: 0, message: 'Мінімум 0'},
+                    max: {value: 1000000, message: 'Максимум 1000000'}
+                })}/>
+            {errors.price && <span>{errors.price.message}</span>}
+
             <input type="text" placeholder={'year'} {...register('year',
-                {valueAsNumber: true})}/>
+                {
+                    valueAsNumber: true,
+                    min: {value: 1990, message: 'Мін 1990'},
+                    max: {value: new Date().getFullYear(), message: `Max ${new Date().getFullYear()}`}
+                })}/>
+            {errors.year && <span>{errors.year.message}</span>}
             <button>Save</button>
         </form>
     );
@@ -36,6 +47,6 @@ export {CarForm};
 //register - to connect inputs to Form hook
 //handleSubmit - to create submit in order to create obj after validation
 //reset - to reset form
-//formState:{errors, isValid} - to have errors and to fin out of the form is or isn't valid
+//formState:{errors, isValid} - to have errors and to find out if the form is or isn't valid
 //setValue - to set/change some values to our inputs
-//useForm({mode:'all'}) - props: mode, to point of what props our error Obj is going to fill in
+//useForm({mode:'all'}) - props: mode, to point what kind of props our error Obj is going to fill in
